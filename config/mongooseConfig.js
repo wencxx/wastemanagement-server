e4e89281bd88120f87c60e7f8c86bedbdb8f10d4
Wency Baterna <wncbtrnfl@gmail.com>
2025-03-09 00:21:03 +0800

    connected to mongo atlas

--- config/mongooseConfig.js
+++ config/mongooseConfig.js
@@ -2,12 +2,13 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/');
+        await mongoose.connect('mongodb+srv://gooteen6:XYZ2001konabatahaha@cluster0.ui6yy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
         console.log('MongoDB connected...');
     } catch (err) {
         console.error(err.message);
         process.exit(1);
     }
 };
+
 
 module.exports = connectDB;
